bdf6bde0a7eba80cb7ec04dd5b0a468f3eb3745b
Rikki407 <rishablamba407@gmail.com>
2018-02-17 17:19:52 +0530

    Authenticatio process setup

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.get('/',function (req, res) {
    res.render("home");
 });
 
-app.get('/secret',function (req, res) {
+app.get('/secret',isLoggedIn,function (req, res) {
    res.render("secret")
 });
 
@@ -72,7 +72,18 @@ app.post("/login",passport.authenticate("local",{
     }),function (req, res) {
 });
 
+//LOGOUT ROUTE
+app.get("/logout",function (req, res) {
+    req.logout();
+    res.redirect("/");
+});
 
+function isLoggedIn(req, res, next) {
+  if(req.isAuthenticated()){
+      return next();
+  }
+  res.redirect("/login");
+}
 
 app.listen(3000, function () {
    console.log("Auth Demo started on port 3000");
