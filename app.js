82389fd4615f18685ae8cc86a0487fc883904f95
Rikki407 <rishablamba407@gmail.com>
2018-02-17 16:01:07 +0530

    setting up passport

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
-var express         = require("express");
-var mongoose        = require("mongoose");
+var express                 = require("express"),
+    mongoose                = require("mongoose"),
+    passport                = require("passport"),
+    bodyParser              = require("body-parser"),
+    Users                   = require("./models/user"),
+    LocalStratergy          = require("passport-local"),
+    passportLocalMongoose   = require("passport-local-mongoose");
 
-var app = express();
 mongoose.connect("mongodb://localhost/auth_demo_app");
 
+var app = express();
+
+app.use(require("express-session")({
+    secret : "Rusty is the best and the cutest dog in the world",
+    resave : false,
+    saveUninitialized : false
+}));
 app.set("view engine", "ejs");
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(Users.serializeUser());
+passport.deserializeUser(Users.deserializeUser());
+
 
 app.get('/',function (req, res) {
    res.render("home");
